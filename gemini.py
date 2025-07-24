05bbdb3b06bb67fe844c65ebc663d006245b3ca3
gronz <gianronz12@gmail.com>
2025-07-24 11:13:23 +0200

    token metrics and add model

--- gemini.py
+++ gemini.py
@@ -1,11 +1,45 @@
+import json
 import os
-
+import time
+import csv
 from google import genai
+from google.genai import types
+def run(model="gemini-2.5-flash",thinking=False):
+    N = 200 #numero di prompt da usare
+    questions = []
+    # Turn off thinking:
+    t = types.ThinkingConfig(thinking_budget=0)
+    if thinking:
+        #thinking dinamico, il modello sceglio quando e quanto pensare
+        t=types.ThinkingConfig(thinking_budget=-1)
+    with open("nq-dev-sample.json", 'r') as f:
+        for i, line in enumerate(f):
+            if i >= N:
+                break
+            item = json.loads(line)
+            questions.append(item['question'])
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    with open(f"{model}_{thinking}.csv","w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["question", "answer", "clock_time","prompt_token","candidates_token","thoughtsTokenCount","totalTokenCount"])
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+        for q in questions:
+            start = time.time()
+            response = client.models.generate_content(
+                model=model, contents=q,
+                config=types.GenerateContentConfig(
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+                    thinking_config=t
+                ),
+            )
+            end = time.time()
+            answer = response.text.strip().replace("\n", " ")
+            usage = response.usage_metadata
+            writer.writerow([q, answer, end - start,usage.prompt_token_count,usage.candidates_token_count,usage.thoughtsTokenCount,usage.total_token_count])
+
+if __name__ == "__main__":
+    run("gemini-2.5-flash",False)
+    run("gemini-2.5-flash",True)
+    run("gemini-2.5-flash-lite",False)
+    run("gemini-2.5-flash",True)
--- localTimes.py
+++ localTimes.py
@@ -1,11 +1,20 @@
-from vllm import LLM
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
-llm = LLM(model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
+#"deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+import json
+import time
+import csv
+from vllm import LLM, SamplingParams
+N = 100 #numero di prompt da usare
+questions = []
+with open("nq-dev-sample.json", 'r') as f:
+    for i, line in enumerate(f):
+        if i >= N:
+            break
+        item = json.loads(line)
+        questions.append(item['question'])
+
+
+llm = LLM(model="Qwen/Qwen2.5-7B-Instruct-GPTQ-Int4",
+          task="generate",
           gpu_memory_utilization=0.85,
           max_model_len=4096,
           max_num_seqs=2,
@@ -13,10 +22,22 @@ llm = LLM(model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
           swap_space=2,
           enable_prefix_caching=True,
         )
-outputs = llm.generate(prompts)
-f = open("output.txt","w")
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    f.write(f"Prompt: {prompt!r}, Generated text: {generated_text!r}\n")
-f.close()
+with open("output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["question", "answer", "clock_time", "total_latency", "ttft","token_prompt","token_out"])
+    for q in questions:
+        start = time.time()
+        s = SamplingParams(max_tokens=128)
+        output = llm.generate([q],sampling_params=s)[0]
+        end = time.time()
+        prompt = output.prompt
+        generated_text = output.outputs[0].text.strip().replace("\n", " ")
+        metrics = output.metrics
+        if metrics and metrics.finished_time and metrics.arrival_time:
+            total_latency = metrics.finished_time - metrics.arrival_time
+        else:
+            total_latency = None
+
+        ttft = metrics.first_token_time if metrics and metrics.first_token_time else None
+
+        writer.writerow([q, generated_text, end - start, total_latency, ttft,len(output.prompt_token_ids),len(output.outputs[0].token_ids)])
